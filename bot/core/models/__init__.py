b7d45a0d1f83d6be67c6dc921ebd04f3b12222f0
theaihopgg <theaihopgg@gmail.com>
2025-06-05 11:53:25 +0700

    feat: added imports to models init file

---
+++ bot/core/models/__init__.py
@@ -0,0 +1,9 @@
+from .base import Base
+from .blacklist_users import BlacklistUserModel
+from .feedbacks import FeedbackModel
+
+__all__ = [
+    "BlacklistUserModel",
+    "FeedbackModel",
+    "Base",
+]
