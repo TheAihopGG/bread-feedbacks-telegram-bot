80822c5d310c1100cba3a85165b1f1d9d6600df5
theaihopgg <theaihopgg@gmail.com>
2025-06-05 19:32:04 +0700

    feat: temporary deleted log models

--- bot/core/models/__init__.py
+++ bot/core/models/__init__.py
@@ -2,8 +2,19 @@ from .base import Base
 from .blacklist_users import BlacklistUserModel
 from .feedbacks import FeedbackModel
 
+# from .blacklist_user_added import BlacklistUserAddedLogModel
+# from .blacklist_user_deleted import BlacklistUserDeletedLogModel
+# from .feedback_created_logs import FeedbackCreatedLogModel
+# from .feedback_updated_logs import FeedbackUpdatedLogModel
+# from .feedback_deleted_logs import FeedbackDeletedLogModel
+
 __all__ = [
     "BlacklistUserModel",
     "FeedbackModel",
     "Base",
+    # "BlacklistUserAddedLogModel",
+    # "BlacklistUserDeletedLogModel",
+    # "FeedbackCreatedLogModel",
+    # "FeedbackUpdatedLogModel",
+    # "FeedbackDeletedLogModel",
 ]
