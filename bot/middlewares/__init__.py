061357f0195e6c05b7d9393d8fa8d60ee04bb081
theaihopgg <theaihopgg@gmail.com>
2025-06-05 16:46:11 +0700

    feat: middlewares init file

---
+++ bot/middlewares/__init__.py
@@ -0,0 +1,7 @@
+from .admin_filter import AdminFilterMiddleware
+from .blacklist_filter import BlackListFilterMiddleware
+
+__all__ = [
+    "AdminFilterMiddleware",
+    "BlackListFilterMiddleware",
+]
