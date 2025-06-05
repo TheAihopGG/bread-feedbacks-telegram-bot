59cbf9ff2e7d331cb4effd57d21d2ea5ef227c5b
theaihopgg <theaihopgg@gmail.com>
2025-06-05 19:34:52 +0700

    fix: feedback crud

--- bot/services/feedback/crud.py
+++ bot/services/feedback/crud.py
@@ -1,50 +1,53 @@
 from sqlalchemy import select, update
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from ...core.database import sessionmaker
 from ...core.system_logger import async_system_logger_decorator
 from ...core.models import FeedbackModel
 
 
-@async_system_logger_decorator()
 async def send_feedback(
+    session: AsyncSession,
     message: str,
     rate: int,
     will_he_buy_more: bool,
     author_telegram_id: int,
 ) -> bool:
-    async with sessionmaker() as session:
-        if (
-            await session.execute(
-                select(
-                    FeedbackModel,
-                ).where(
-                    FeedbackModel.author_telegram_id == author_telegram_id,
-                ),
+    if (
+        await session.execute(
+            select(
+                FeedbackModel,
+            ).where(
+                FeedbackModel.author_telegram_id == author_telegram_id,
+            ),
+        )
+    ).scalar_one_or_none():
+        result = await session.execute(
+            update(
+                FeedbackModel,
             )
-        ).scalar_one_or_none():
-            result = await session.execute(
-                update(
-                    FeedbackModel,
-                )
-                .values(
-                    content=message,
-                )
-                .where(
-                    FeedbackModel.author_telegram_id == author_telegram_id,
-                ),
+            .values(
+                message=message,
             )
-            return bool(result.rowcount)
-        else:
-            session.add(
-                FeedbackModel(
-                    message=message,
-                    rate=rate,
-                    will_he_buy_more=will_he_buy_more,
-                    author_telegram_id=author_telegram_id,
-                )
+            .where(
+                FeedbackModel.author_telegram_id == author_telegram_id,
+            ),
+        )
+        await session.commit()
+
+        return bool(result.rowcount)
+    else:
+        session.add(
+            FeedbackModel(
+                message=message,
+                rate=rate,
+                will_he_buy_more=will_he_buy_more,
+                author_telegram_id=author_telegram_id,
             )
-            await session.commit()
-            return True
+        )
+        await session.commit()
+
+        return True
 
 
 __all__ = [
